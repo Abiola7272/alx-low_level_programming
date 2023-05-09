b0e285b44888b81a098407d4206dfe1890961414
abiola7272 <moshoodabiola7272@gmail.com>
2023-05-08 20:51:52 -0700

    File Input and Output Task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- It reads the text file printed to Standard Output (STDOUT).
+ * @filename: The already read text file.
+ * @letters: THe number of letters to be read on the text file.
+ * Return: w- It is the actual number of bytes read and printed.
+ *	0 is retuned when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *nuf;
+	ssize_t md;
+	ssize_t w;
+	ssize_t t;
+
+	md = open(filename, O_RDONLY);
+	if (md == -1)
+	{
+		return (0);
+	}
+
+	nuf = malloc(sizeof(char) * letters);
+	t = read(md, nuf, letters);
+	w = write(STDOUT_FILENO, nuf, t);
+
+	free(nuf);
+	close(md);
+	return (w);
+}
